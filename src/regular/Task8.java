9c11457d42c6e5909f0e59f46b6e543873f450bd
RBarnabass <rastafarimood@gmail.com>
2018-06-15 16:20:18 +0300

    again

--- src/regular/Task8.java
+++ src/regular/Task8.java
@@ -11,25 +11,21 @@ import java.util.Scanner;
         18     98   -   true
 */
 
-public class Task8
-{
-    public static void main(String[] args)
-    {
+public class Task8 {
+    public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter two integer numbers, please: ");
         int num1 = scan.nextInt();
         int num2 = scan.nextInt();
-        boolean isEqual = true;
         int lastNumber1 = num1 % 10;
         int lastNumber2 = num2 % 10;
 
-        if (lastNumber1 == lastNumber2)
-        {
-            System.out.println(isEqual);
+        if (lastNumber1 == lastNumber2) {
+            System.out.println(true);
         }
-        else
-        {
-            System.out.println(!isEqual);
+        else {
+            System.out.println(false);
         }
     }
 }
