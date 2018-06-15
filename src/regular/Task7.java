dcfb0f455dd44755d60ec87c2c453537eb4ba49a
RBarnabass <rastafarimood@gmail.com>
2018-06-15 16:13:11 +0300

    new one

--- src/regular/Task7.java
+++ src/regular/Task7.java
@@ -7,43 +7,26 @@ import java.util.Scanner;
         В іншому випадку виводимо false і результат ділення (цілу частину і залишок).
 */
 
-public class Task7
-{
-    public static void main(String[] args)
-    {
+public class Task7 {
+    public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter two integer numbers, please: ");
         int num1 = scan.nextInt();
         int num2 = scan.nextInt();
         boolean isDivisable = false;
 
-        if (num1 == 0 || num2 == 0)
-        {
-            System.out.println("Division of zero is impossible");
-
-            if (num1 >= num2)
-            {
-                if (num1 % num2 == 0)
-                {
-                    System.out.println(!isDivisable);
-                    System.out.println("The result of divisible " + (num1 / num2));
-                }
-                else
-                    {
-                    System.out.println(isDivisable);
-                    System.out.println("The result of divisible " + (num1 / num2) + "," + (num1 % num2));
-                }
-            }
-            else if (num2 % num1 == 0)
-            {
-                System.out.println(!isDivisable);
-                System.out.println("The result of divisible " + (num2 / num1));
-            }
-            else
-                {
-                System.out.println(isDivisable);
-                System.out.println("The result of divisible " + (num2 / num1) + "," + (num2 % num1));
-            }
+        if (num1 % num2 == 0) {
+            System.out.println(!isDivisable);
+            System.out.println("The result of divisible " + (num1 / num2));
+        }
+        else if (num2 % num1 == 0) {
+            System.out.println(!isDivisable);
+            System.out.println("The result of divisible " + (num2 / num1));
+        }
+        else if (num1 % num2 != 0) {
+            System.out.println(isDivisable);
+            System.out.println("The result of divisible " + num1 / num2 + "," + num1 % num2);
         }
     }
 }
